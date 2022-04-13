3912e33c538de4d036cc63a5bb9279bb22b00645
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2022-04-13 12:14:03 +0530

    Create Kth smallest element .cpp

---
+++ Kth smallest element .cpp
@@ -0,0 +1,11 @@
+int kthSmallest(int arr[], int l, int r, int k) 
+    {
+        priority_queue<int>pq;
+        for(int i=0;i<=r;i++)
+        {
+            pq.push(arr[i]);
+            if(pq.size()>k)
+               pq.pop();
+        }
+        return pq.top();
+    }
