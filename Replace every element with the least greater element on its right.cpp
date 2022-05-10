8f7721d7916d561000783405e5c5e30dc56f632b
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2022-05-10 15:00:45 +0530

    Create Replace every element with the least greater element on its right.cpp

---
+++ Replace every element with the least greater element on its right.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+    public:
+    vector<int> findLeastGreater(vector<int>& arr, int n) 
+    {
+        set<int>s;
+        vector <int>v(n,-1);
+        for(int i=n-1; i>=0; i--)
+        {
+            auto it=s.upper_bound(arr[i]);
+            if(it!=s.end())
+                v[i]=*it;
+            s.insert(arr[i]);
+        }
+        return v;    
+    }
+};
