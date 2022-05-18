456d14f73a817d88d6ab97a79b7fd2ee218e9221
Prabhat Mishra <54847117+Prabhat2002@users.noreply.github.com>
2022-05-18 10:40:38 +0530

    Update Count total set bits.cpp

--- Count total set bits.cpp
+++ Count total set bits.cpp
@@ -1,3 +1,5 @@
+Method 1 :
+
 class Solution
 {
     public:
@@ -17,3 +19,25 @@ class Solution
           return (1<<(x-1))*x + (n- (1<<x) +1 ) + countSetBits( n- (1<<x));
     }
 };
+
+Method 2 :
+class Solution
+{
+    public:
+    int largestPowerOfTwo(int n)
+    {
+      int x=0;
+       while((pow(2,x))<=n)
+          x++;
+       return x-1;
+    }
+    int countSetBits(int n)
+    {
+      int x=largestPowerOfTwo(n);
+      if(n==0)
+          return 0;
+      else 
+          return (pow(2,x-1))*x + (n- (pow(2,x)) +1 ) + countSetBits( n- (pow(2,x)));
+    }
+};
+
